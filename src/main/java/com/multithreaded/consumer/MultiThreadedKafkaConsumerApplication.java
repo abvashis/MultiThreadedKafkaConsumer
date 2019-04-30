be0da74d13db3d6f43f397f752231f34adbff243
Abhishek Vashistha <vashistha.abhishek@gmail.com>
2019-04-30 14:58:14 +0530

    done

---
+++ src/main/java/com/multithreaded/consumer/MultiThreadedKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.multithreaded.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadedKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreadedKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/multithreaded/consumer/multipleconsumers/readme.txt
@@ -0,0 +1,3 @@
+https://howtoprogram.xyz/2016/05/29/create-multi-threaded-apache-kafka-consumer/
+
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic HelloKafkaTopic1
